b81accaa52ddfd02ab4e50e902b7a5548364ea70
Pedro L <pedrocontact22@gmail.com>
2022-11-17 14:43:36 -0300

    Fixed vertical sizing for login and register screens

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -3,37 +3,39 @@ import Link from "next/link";
 
 export default function Login() {
   return (
-    <div className="flex">
-      <form
-        action=""
-        className="mx-auto my-[10vw] px-10 pt-10 pb-14 bg-white rounded-md shadow-xl"
-      >
-        <h1 className="text-center text-2xl font-semibold text-gray-600 pb-5">
-          Login
-        </h1>
-        <input
-          type="text"
-          placeholder="Username"
-          className="block px-5 py-2 my-1 rounded-md bg-gray-100 outline-none focus:bg-gray-200 text-gray-500"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="block px-5 py-2 my-1 rounded-md bg-gray-100 outline-none focus:bg-gray-200 text-gray-500"
-        />
-        <button
-          type="submit"
-          className="flex mx-auto my-7 text-center bg-violet-400 hover:bg-violet-500 text-white px-[41%] py-2 rounded-md transition-all duration-250 ease-in"
+    <div className="bg-gray-300 h-screen">
+      <main className="flex">
+        <form
+          action=""
+          className="mx-auto my-[10vw] px-10 pt-10 pb-14 bg-white rounded-md shadow-xl"
         >
-          Login
-        </button>
-        <p className="text-sm mt-14 text-center">
-          {"Don't have an account yet? "}{" "}
-          <Link href="/register" className="text-violet-400 hover:underline">
-            Register
-          </Link>
-        </p>
-      </form>
+          <h1 className="text-center text-2xl font-semibold text-gray-600 pb-5">
+            Login
+          </h1>
+          <input
+            type="text"
+            placeholder="Username"
+            className="block px-5 py-2 my-1 rounded-md bg-gray-100 outline-none focus:bg-gray-200 text-gray-500"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="block px-5 py-2 my-1 rounded-md bg-gray-100 outline-none focus:bg-gray-200 text-gray-500"
+          />
+          <button
+            type="submit"
+            className="flex mx-auto my-7 text-center bg-violet-400 hover:bg-violet-500 text-white px-[41%] py-2 rounded-md transition-all duration-250 ease-in"
+          >
+            Login
+          </button>
+          <p className="text-sm mt-14 text-center">
+            {"Don't have an account yet? "}{" "}
+            <Link href="/register" className="text-violet-400 hover:underline">
+              Register
+            </Link>
+          </p>
+        </form>
+      </main>
     </div>
   );
 }
