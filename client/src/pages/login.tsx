176dc8543923eeeecf4b108bcc7cffa9cae082ef
Pedro L <pedrocontact22@gmail.com>
2022-11-17 17:24:07 -0300

    Update login.tsx

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,40 +1,21 @@
-import React, { useContext } from "react";
+import React from "react";
 import Link from "next/link";
-import { useForm } from "react-hook-form";
-import { AuthContext } from "../context/AuthContext";
 
 export default function Login() {
-  const { register, handleSubmit } = useForm();
-
-  const { signIn } = useContext(AuthContext);
-
-  async function handleSignIn(data: any) {
-    try {
-      signIn(data);
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
   return (
     <div className="bg-gray-300 h-screen">
       <main className="flex">
-        <form
-          onSubmit={handleSubmit(handleSignIn)}
-          className="mx-auto my-[10vw] px-10 pt-10 pb-14 bg-white rounded-md shadow-xl"
-        >
+        <form className="mx-auto my-[10vw] px-10 pt-10 pb-14 bg-white rounded-md shadow-xl">
           <h1 className="text-center text-2xl font-semibold text-gray-600 pb-5">
             Login
           </h1>
           <input
-            {...register("username")}
             type="text"
             name="username"
             placeholder="Username"
             className="block px-5 py-2 my-1 rounded-md bg-gray-100 outline-none focus:bg-gray-200 text-gray-500"
           />
           <input
-            {...register("password")}
             type="password"
             name="password"
             placeholder="Password"
